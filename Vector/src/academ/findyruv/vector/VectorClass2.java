7df156770c63ee1a824a4c9e78c2f51394f08341
Никита <nik.findyurov@mail.ru>
2022-08-07 14:45:49 +0700

    New commit

---
+++ Vector/src/academ/findyruv/vector/VectorClass2.java
@@ -0,0 +1,14 @@
+package academ.findyruv.vector;
+
+public class VectorClass2 extends VectorClass {
+    private final VectorClass vector2;
+
+    public VectorClass2(int n, VectorClass vector) {
+        super(n);
+        this.vector2 = vector;
+    }
+
+    public VectorClass getVector2() {
+        return vector2;
+    }
+}
---
+++ Vector/src/academ/findyruv/vector/VectorClass3.java
@@ -0,0 +1,25 @@
+package academ.findyruv.vector;
+
+import java.util.Vector;
+
+public class VectorClass3 extends VectorClass {
+    private Vector<Double> vector3;
+
+    public VectorClass3(int n, double[] mas) {
+        super(n);
+
+        int i = 0;
+        while (n < mas.length) {
+            vector3.add(mas[i]);
+            i++;
+        }
+    }
+
+    public Vector<Double> getVector3() {
+        return vector3;
+    }
+
+    public void setVector3(Vector<Double> vector3) {
+        this.vector3 = vector3;
+    }
+}
