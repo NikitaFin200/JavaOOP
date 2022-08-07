34e00a239e206769afb9136df047548e75926cc4
Никита <nik.findyurov@mail.ru>
2022-08-07 14:31:20 +0700

    New commit

---
+++ Shapes/src/academ/findyurov/shapes/Shapes.java
@@ -0,0 +1,13 @@
+package academ.findyurov.shapes;
+
+public interface Shapes {
+    double getWidth();
+
+    double getHeight();
+
+    double getArea();
+
+    double getPerimeter();
+
+    String name();
+}
